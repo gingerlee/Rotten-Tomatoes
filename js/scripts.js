d6f07b56fab94623fb4b35575490a34385928e25
Ginger Lee Kretschmer and Lew Davidson <lew@>
2017-11-16 14:54:01 -0800

    add sort function

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,31 @@
+function Movie(date, title, score) {
+  this.date = date;
+  this.title = title;
+  this.score = score;
+}
+var movies = [];
+
 $(document).ready(function() {
-  
+  $(".form").submit(function(event) {
+    event.preventDefault();
+    var date = $("input#date").val();
+    var title = $("input#title").val();
+    var score = parseInt($("input#score").val());
+    var newMovie = new Movie(date, title, score);
+
+    $(".tbody").append("<tr>" + "<td>" + newMovie.date + "</td>" + "<td>" + newMovie.title + "</td>" + "<td>" + newMovie.score + "</td>" + "</tr>");
+
+    movies.push(newMovie);
+  });
+
+  $("#score-sort").click(function() {
+    $(".tbody").empty();
+      movies.sort(function(a, b){
+      return b.score - a.score;
+      });
+
+    movies.forEach(function(movie) {
+    $(".tbody").append("<tr>" + "<td>" + movie.date + "</td>" + "<td>" + movie.title + "</td>" + "<td>" + movie.score + "</td>" + "</tr>");
+    });
+  });
 });
